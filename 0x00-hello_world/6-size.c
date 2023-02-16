82970391edab0251dcc477ca6e3dc617da225d13
EvansSese <sese_evans@yahoo.com>
2023-02-16 08:37:14 -0800

    Added file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long_type));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_type));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
-	
 	return (0);
 }
