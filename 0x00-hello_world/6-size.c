98dfb65032009da9455234a2873ce6a82c288ed4
EvansSese <sese_evans@yahoo.com>
2023-02-16 08:34:28 -0800

    Added file to print size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 	int int_type;
 	long long_type;
 	float float_type;
-
 	printf("Size of a char: %zu bytes(s)\n", sizeof(charType));
 	printf("Size of an int: %zu bytes(s)\n", sizeof(int_type));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long_type));
@@ -18,4 +17,3 @@ int main(void)
 	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
 	return (0);
 }
-
