3ed7c02c7c7018b9c6285e7472d240c2bd74459b
EvansSese <sese_evans@yahoo.com>
2023-02-21 08:01:16 -0800

    Added alphabet program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
