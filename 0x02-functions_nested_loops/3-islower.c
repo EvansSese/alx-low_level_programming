2cef0179fe24d14e4947e44790afa9336cc744eb
EvansSese <sese_evans@yahoo.com>
2023-02-22 07:53:24 -0800

    Added function to determine if is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * islower - Returns 1 if c is lower, otherwise Returns 0
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return(1);
+	return (0);
+}
