2125a898e16e481c0fcb7a444b6957fafb9a2e30
EvansSese <sese_evans@yahoo.com>
2023-02-22 10:51:25 -0800

    added function to print sign of a number given

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,12 @@
 /**
  * print_sign - Prints the sign infornt of a given number
  * @n: Number to be checked
- * Return: 1 if number is positive, 0 if number is 0 and -1 if number is negative
+ * Return: 1 if number is positive, 0 if number is 0, -1 if number is negative
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
 		return (1);
 	}
 	else if (n < 0)
