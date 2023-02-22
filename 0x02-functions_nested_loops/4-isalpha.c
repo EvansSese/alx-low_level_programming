3e2a00e942ff76a8615e0e7cd2142f3104584649
EvansSese <sese_evans@yahoo.com>
2023-02-22 08:37:04 -0800

    Added function to determine if a character is a letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,13 @@
 /**
  * _isalpha - checks if a given character is a letter, lowercase or uppercase
  * @c: The character to be checked
- * Retrun: 1 if character is a letter, otherwise 0
+ * Return: 1 if character is a letter, otherwise 0
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	       return (1);
+	{
+		return (1);
+	}
 	return (0);
-}	
+}
