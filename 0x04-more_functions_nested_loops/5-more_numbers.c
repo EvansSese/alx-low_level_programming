311d84b973c7c11bcd8196f88d2dba732d3ab957
EvansSese <sese_evans@yahoo.com>
2023-02-23 06:02:47 -0800

    Added files

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,19 @@
  */
 void more_numbers(void)
 {
-	int n, i;
+	int n;
+	int i;
 
 	for (n = 0; n < 10; n++)
 	{
 		for (i = 0; i <= 14; i++)
-			_putchar(i + '0');
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
