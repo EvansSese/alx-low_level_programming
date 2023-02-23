de2711f90600451852b0f26311fc26ce497154da
EvansSese <sese_evans@yahoo.com>
2023-02-23 04:41:24 -0800

    Added files

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,20 +3,14 @@
 /**
  * _isdigit - Determines if a number is a digit
  * @c: Character to be checked
- * @i: Number in the for loop
  *
  * Return: 1 if is digit. 0 otherwise
  */
 int _isdigit(int c)
 {
-	int i;
-
-	for (i = 0; i <= 9; i++)
+	if (c >= '0' && c <= '9')
 	{
-		if (i == c)
-		{
-			return (1);
-		}
+		return (1);
 	}
 	return (0);
 }
