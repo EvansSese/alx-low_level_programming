769cdf230048810abc75f451f664a58b9da3c45e
EvansSese <sese_evans@yahoo.com>
2023-02-24 02:20:46 -0800

    Added files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @r - variable to hold return value
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	return (0);
+}
