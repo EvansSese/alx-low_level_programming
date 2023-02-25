54444c8c7f6804e074007032907ab6e351013bcb
EvansSese <sese_evans@yahoo.com>
2023-02-25 03:41:56 -0800

    Added correction to function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,6 @@ void times_table(void)
 	{
 		for (mult = 0; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			prod = num * mult;
 
 			if (prod <= 9)
@@ -25,6 +22,11 @@ void times_table(void)
 				_putchar((prod / 10) + '0');
 			}
 			_putchar((prod % 10) + '0');
+			if (mult < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
