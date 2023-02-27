f147c742417a2d057a0ade57248a7dc7b37ef9db
EvansSese <sese_evans@yahoo.com>
2023-02-27 08:45:36 -0800

    Added function to print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
  */
 void print_rev(char *s)
 {
-	char [] = s;
+	char str[] = s;
 	int len;
 	int i;
 
 	len = strlen(s);
 	for (i = 0; i < len; i++)
 	{
-		_putchar(s[i] + '0');
+		_putchar(str[i] + '0');
 	}
 }
