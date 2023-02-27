5eecde54b67becbc265aa50010cc73bd74b8dae0
EvansSese <sese_evans@yahoo.com>
2023-02-27 07:24:33 -0800

    Added function to swap 2 values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - Swaps the values of 2 integers
+ * @a: Value for a
+ * @b: Value for b
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
