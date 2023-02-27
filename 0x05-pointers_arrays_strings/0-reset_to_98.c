53c2f257b2c88cb6fa3f99c03a17ef0180d0af05
EvansSese <sese_evans@yahoo.com>
2023-02-27 07:19:52 -0800

    Added function to reset value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include "main.h"
 /**
  * reset_to_98 - Defaults value to 98
- * @*n: Pointer to memory location for n
+ * @n: Pointer to memory location for n
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
-}	
+}
