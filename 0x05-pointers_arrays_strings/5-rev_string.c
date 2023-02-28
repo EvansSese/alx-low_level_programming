aac3e09fb45f11701b90362afb69e6619323514e
EvansSese <sese_evans@yahoo.com>
2023-02-27 23:58:18 -0800

    Added function to rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,11 @@ void rev_string(char *s)
 	int len = strlen(s);
 	int i = len -1;
 	int j;
+	char s[1];
 
 	for (j = 0; j < len; j++)
 	{
-		printf("%c", s[i]);
+		strcat(s, s[j]);
 		i--;
 	}
-	printf("\n");
 }
