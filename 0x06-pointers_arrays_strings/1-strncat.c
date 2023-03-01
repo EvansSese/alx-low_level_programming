3e165d6ca9af1f47c051e1a3a797d4f583ce6f4c
EvansSese <sese_evans@yahoo.com>
2023-03-01 04:30:02 -0800

    Added function to concatenate strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strncat - Function to concatenate string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of chars to be cancatenated
+ *
+ * Return: Returns string
+ */
+char *strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
