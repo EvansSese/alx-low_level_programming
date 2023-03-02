7e89a0b3a39983b5c00efd4ee244909781516307
EvansSese <sese_evans@yahoo.com>
2023-03-02 06:36:12 -0800

    Added function to convert to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,8 +2,7 @@
 #include <string.h>
 #include "main.c"
 /**
- * string_toupper - Function to convert 
- * lowercase to upper
+ * string_toupper - Func to convert case
  * @str: String parameter
  *
  * Return: Returns a pointer
