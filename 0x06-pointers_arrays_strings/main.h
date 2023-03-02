8bcbeb6b53f477eb5a7d564284746cfe4ad8623e
EvansSese <sese_evans@yahoo.com>
2023-03-02 07:26:45 -0800

    Added function to encode string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-ifndef FILE_MAIN
-#define FILE_MAIN
-
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,5 +7,3 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *);
 char *leet(char *);
-
-#endif
