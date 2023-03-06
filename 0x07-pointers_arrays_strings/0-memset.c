b384dd5e4b27b1ebf8db0e887060eb2c172d912f
EvansSese <sese_evans@yahoo.com>
2023-03-06 06:18:44 -0800

    Added function to fill up memory area with 1 byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _memset - Function to fill memory with 1 byte
+ * @s: String parameter
+ * @b: Character parameter
+ * @n: Integer parameter
+ *
+ * Return: Returns a pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(char *S, char b, unsigned int n));
+}
