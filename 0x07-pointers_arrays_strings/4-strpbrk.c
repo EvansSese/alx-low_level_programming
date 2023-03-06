b598a127b100d688b18cd9aa260f12cc0c4fc6cc
EvansSese <sese_evans@yahoo.com>
2023-03-06 08:12:05 -0800

    Added function that returns a pointer to the byte in s

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strpbrk - Function that searches a string for any set of bytes
+ * @s: String parameter
+ * @accept: String
+ *
+ * Return: Pointer to the byte s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+
+	while (*s != '\0')
+	{
+		p = accept;
+		while (*p != '\0')
+		{
+			if (*s == *p)
+			{
+				return s;
+			}
+			p++;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
