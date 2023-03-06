37b9e0d95df9a054c3060106ee65a1e13af4cf70
EvansSese <sese_evans@yahoo.com>
2023-03-06 13:29:01 -0800

    Added function to print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_chessboard - Function to print chessboard
+ * @a: Array containing chess figures
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	int len = sizeof(*a) / sizeof((*a)[0]);
+
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < len; j++)
+		{
+			_putchar((*a)[j] + '0');
+		}
+		_putchar('\n');
+	}
+}
