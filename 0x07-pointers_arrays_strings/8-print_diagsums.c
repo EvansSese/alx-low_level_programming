aa345f57384e80c8fbb7d02b1ab87b9cc907e39f
EvansSese <sese_evans@yahoo.com>
2023-03-07 04:15:17 -0800

    Added function to find sum of diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,13 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int j = size - 1;
 	int leftSum = 0;
 	int rightSum = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		leftSum = leftSum + a[i][i];
-		rightSum = rightSum + a[i][j];
+		leftSum += *(a + i * size + i);
+		rightsum += *(a + i * size + (size - i - 1));
 	}
 	printf("%d, %d", leftSum, rightSum);
 }
