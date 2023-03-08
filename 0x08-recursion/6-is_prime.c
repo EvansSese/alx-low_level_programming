222b4a2552754d7f07c2ccdbf018e83779a2cfeb
EvansSese <sese_evans@yahoo.com>
2023-03-08 14:57:25 -0800

    Added function to check for prime number

---
+++ 0x08-recursion/6-is_prime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * is_prime_number - Function to check prime number
+ * @n: Number to be checked
+ * @i: Index
+ *
+ * Return: 1 if prime, else 0
+ */
+int is_divisible(int n, int i);
+int is_divisible(int n, int i)
+{
+	if (i * i > n)
+		return (0);
+	else if (n % i == 0)
+		return (1);
+	else
+		return is_divisible(n, i + 1);
+}
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	else if (n == 2)
+		return (1);
+	else if (n % 2 == 0)
+		return (0);
+	else
+		return (!is_divisible(n, 3));
+}
