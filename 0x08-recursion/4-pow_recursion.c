00208bd4dbfdb90335d4442e1974782f6efebb47
EvansSese <sese_evans@yahoo.com>
2023-03-08 14:11:58 -0800

    Added function to raise power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - Function to return power
+ * @x: Base
+ * @y: Power
+ *
+ * Return: Resulting power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
