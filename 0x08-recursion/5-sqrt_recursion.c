d4b2ca42036fac922881ebe8f57538dbf7228cf5
EvansSese <sese_evans@yahoo.com>
2023-03-08 14:35:07 -0800

    Added function to find sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _sqrt_recursion - Function to find root
+ * @n: Number
+ *
+ * Return: The sqrt
+ */
+int _sqrt_helper(int n, int low, int high);
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt_helper(n, 0, n));
+}
+int _sqrt_helper(int n, int low, int high)
+{
+	if (low > high)
+		return (-1);
+	
+	int mid = (low + high) / 2;
+	int guess = mid * mid;
+
+	if (guess == n)
+		return (mid);
+	else if (guess > n)
+		return (_sqrt_helper(n, low, mid - 1));
+	else
+		return (_sqrt_helper(n, mid + 1, high));
+}
