e93eabc28fb0997cd1a83cc5bdf8a451c84b2eb8
EvansSese <sese_evans@yahoo.com>
2023-03-08 13:54:23 -0800

    Added function to find the len of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s != '\0')
-	{
-		len++;
-		return (_strlen_recursion(s + 1));
-	}
+	if (*s == '\0')
+		return (0);
+	return 1 + _strlen_recursion(s + 1);
 }
