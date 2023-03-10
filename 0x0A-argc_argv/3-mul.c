3c3ef10037ff84f53b250c6bb318ef6302c71eda
EvansSese <sese_evans@yahoo.com>
2023-03-10 15:15:13 -0800

    Added file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Arguments
+ * @argv: Arguments
+ *
+ * Return: Retuns integer
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2';
+	printf("%d\n", result;
+	return (0);
+}
