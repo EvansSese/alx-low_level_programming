d966309f8b11e4c7220b62ac8a5b6bf40fb0155c
EvansSese <sese_evans@yahoo.com>
2023-03-10 14:49:58 -0800

    Added file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * @argc: Arguments
+ * @argv: Arguments
+ *
+ * Return: Return number of args
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
