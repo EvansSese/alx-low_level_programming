e7d12c80eb57bdab86fb2e031d8d65daa4216059
EvansSese <sese_evans@yahoo.com>
2023-03-10 14:39:34 -0800

    Added files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Main function
+ * @argc: Arguments
+ * @argv: Arguments
+ *
+ * Return: Returns 0
+ */
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
