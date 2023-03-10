19402da3a6648fec47db6cc36ea3db13987a3942
EvansSese <sese_evans@yahoo.com>
2023-03-10 15:20:29 -0800

    Added file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,12 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error\n");
+		printf("Error");
 		return (1);
 	}
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 	int result = num1 * num2';
-	printf("%d\n", result;
+	printf("%d\n", result);
 	return (0);
 }
