59cf360a8965f900c54ffc46870b81d2ff3f9bf4
EvansSese <sese_evans@yahoo.com>
2023-03-10 15:53:08 -0800

    Added files

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: Argumrents
+ * @argv: Arguments
+ *
+ * Return: Returns intehegfr
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		char *ptr = argv[i];
+		while (*ptr)
+		{
+			if (!isdigit(*ptr))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			ptr++;
+		}
+		sum += atoi argv[i];
+	}
+	printf("%d\n", sum);
+	return (0);
+}
