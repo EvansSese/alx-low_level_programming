30efe52e83221e07577dd39e4d6a2215fe95dcaa
EvansSese <sese_evans@yahoo.com>
2023-03-13 09:59:13 -0700

    Added file to project folder

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 
-	int len1;
-	int len2;
+	size_t len;
 	char *new_str;
 
 	if (s1 == NULL)
@@ -23,13 +22,13 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	new_str = malloc((len1 * sizeof(char)) + (len2 * sizeof(char)) + 1);
+	len = strlen(s1) + strlen(s2) + 1;
+	new_str = malloc(len);
 	if (new_str == NULL)
 	{
 		return (NULL);
 	}
-	new_str = strcat(s1, s2);
+	strcpy(new_str, s1);
+	strcat(new_str, s2);
 	return (new_str);
 }
