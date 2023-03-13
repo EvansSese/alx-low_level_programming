21bb15680aa00ccbbed83092bb4d502119bc4954
EvansSese <sese_evans@yahoo.com>
2023-03-13 09:11:56 -0700

    Added file to project folder

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,17 +9,20 @@
  */
 char *_strdup(char *str)
 {
-	size_t len = strlen(str);
-	char *new_str = malloc(len + 1);
+	if (str != NULL)
+	{
+		size_t len = strlen(str);
+		char *new_str = malloc(len + 1);
 
-	if (str == NULL)
+		if (new_str == NULL)
+		{
+			return (NULL);
+		}
+		memcpy(new_str, str, len + 1);
+		return (new_str);
+	}
+	else
 	{
 		return (NULL);
 	}
-	if (new_str == NULL)
-	{
-		return (NULL);
-	}
-	memcpy(new_str, str, len + 1);
-	return (new_str);
 }
