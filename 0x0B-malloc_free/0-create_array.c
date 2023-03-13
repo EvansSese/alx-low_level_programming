354bd857a701a751cf3c8254fd39f01490510590
EvansSese <sese_evans@yahoo.com>
2023-03-13 08:46:42 -0700

    Added file to project folder

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char* arr = (char*) malloc(size * sizeof(char));
+	char *arr = (char *) malloc(size * sizeof(char));
 
 	if (size == 0)
 	{
