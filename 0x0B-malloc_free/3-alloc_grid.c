ef91886ede51eaa78fa70c324aa1ee7ab75ecd55
EvansSese <sese_evans@yahoo.com>
2023-03-14 01:40:19 -0700

    Added file to project folder

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - Function to create a 2d array
+ * @width: Width of the grid
+ * @height: Height of the grid
+ *
+ * Return: Pointer to the grid or Null
+ */
+int **alloc_grid(int width, int height)
+{
+	int ** grid;
+	int i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = calloc(width, sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
