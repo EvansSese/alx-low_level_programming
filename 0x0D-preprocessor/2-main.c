842d1f2df7ae1dc3148c62b7079c1f832d88f8dc
EvansSese <sese_evans@yahoo.com>
2023-03-20 11:11:54 -0700

    Added file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
-#include  <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - main function that prints file name
  *
  * Return: Return 0
  */
-int main(void)
+int main()
 {
 	printf(__FILE__);
 	return (0);
