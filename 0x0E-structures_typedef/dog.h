e00ad96c8d06d1607056f065a5e28cf7c0003831
EvansSese <sese_evans@yahoo.com>
2023-03-20 13:56:04 -0700

    Added files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Dog datatype
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of the dog
+ *
+ * Description: Dog datatype
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
