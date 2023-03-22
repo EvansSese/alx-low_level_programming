006a34839d25041c27a34ff0165aee5a2fedc521
EvansSese <sese_evans@yahoo.com>
2023-03-22 13:02:47 -0700

    Added files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.c"
+/**
+ * print_name - Function to print name
+ * @name: Name parameter
+ * @f: Punction pointer
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void(*f)(char *))
+{
+	printf("%s", (*f)(name));
+}
