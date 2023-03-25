95b202c10d05ac59fbb91f312e66d3b0e9eecce7
EvansSese <sese_evans@yahoo.com>
2023-03-25 02:27:28 -0700

    Added files

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,4 +1,8 @@
+#ifndef DOG_H
+#define DOG_H
+#include "dog.h"
 struct dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+#endif
