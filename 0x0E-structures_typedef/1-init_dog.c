46d0a5712389656e0903ddcf3222dbb340a987c5
EvansSese <sese_evans@yahoo.com>
2023-03-25 04:02:37 -0700

    Added files

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,6 +11,7 @@
  *
  * Return: Nothing
  */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
