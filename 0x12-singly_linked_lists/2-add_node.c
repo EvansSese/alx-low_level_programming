7d107191b59cd7ac36f992c92ee31dfa6f1b68cc
EvansSese <sese_evans@yahoo.com>
2023-03-30 06:21:34 -0700

    Added files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 /**
  * add_node - Function to add node
@@ -20,5 +21,5 @@ list_t *add_node(list_t **head, const char *str)
 	temp->len = strlen(strdup(str));
 	temp->next = *head;
 	*head = temp;
-	return (head);
+	return (*head);
 }
