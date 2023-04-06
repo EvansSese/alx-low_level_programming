84c928fdf34c85e73295ce4da34c366858128f1a
EvansSese <sese_evans@yahoo.com>
2023-04-06 05:57:15 -0700

    Added files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,4 +9,17 @@
  */
 void print_binary(unsigned long int n);
 {
+	int i;
 
+	for (i = (sizeof(unsigned long int)) - 1; i >= 0; i--)
+	{
+		if (n & (1ul << i))
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
