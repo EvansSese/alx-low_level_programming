7e5a7f5fbd73995fec039ff9bbbad6f39c7fccba
EvansSese <sese_evans@yahoo.com>
2023-04-06 05:49:09 -0700

    Added files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,27 +10,21 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int i = 0;
+	unsigned int bit = 1;
+	const char *end;
 
 	if (b == NULL)
-	{
 		return (0);
-	}
-	while (b[i] != '\0')
+	while (*end != '\0')
+		end++;
+	while (end >= b)
 	{
-		if (b[i] == '0')
-		{
-			result = (result << 1) | 0;
-		}
-		else if (b[i] == 1)
-		{
-			result = (result << 1) | 1;
-		}
-		else
-		{
+		if (*end == '1')
+			result += bit;
+		else if (*end != '0')
 			return (0);
-		}
-		i++;
+		bit <<= 1;
+		end--;
 	}
 	return (result);
 }
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - Prints binary representation of number
+ * @n: Integer
+ *
+ * Return: Returns binary number
+ */
+void print_binary(unsigned long int n);
+{
+
