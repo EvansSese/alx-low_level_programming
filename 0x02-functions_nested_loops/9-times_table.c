afe4b0e37c5caadd154a12ed1d8302aab33c5875
Evans Sese <Sese_evans@yahoo.com>
2023-04-20 11:17:23 +0300

    Updated file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,19 +15,19 @@ void times_table(void)
 
 			if (prod <= 9)
 			{
-				_putchar(' ');
+				putchar(' ');
 			}
 			else
 			{
-				_putchar((prod / 10) + '0');
+				putchar((prod / 10) + '0');
 			}
-			_putchar((prod % 10) + '0');
+			putchar((prod % 10) + '0');
 			if (mult < 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
