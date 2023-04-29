f52e8be20e5b28e68ef45f0f63cc13998bc8eab9
Evans Sese <Sese_evans@yahoo.com>
2023-04-29 17:36:10 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	unsigned int bit = 1;
-	const char *end;
-
 	if (b == NULL)
 		return (0);
-	while (*end != '\0')
-		end++;
-	while (end >= b)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (*end == '1')
-			result += bit;
-		else if (*end != '0')
+		if (b[i] == '0')
+			result = result << 1;
+		else if (b[i] == '1')
+			result = (result << 1) | 1;
+		else
 			return (0);
-		bit <<= 1;
-		end--;
 	}
 	return (result);
 }
