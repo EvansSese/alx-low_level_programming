3379df98436b06ed8eb2b7a7b27daabc31cb55c0
Evans Sese <sese_evans@yahoo.com>
2023-05-04 11:28:21 -0700

    [Feature] Added function to get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_endianness - Gets endiannes
+ *
+ * Return: 0 for big endian, 1 for little endian
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *ptr = (char *) &num;
+
+	if (*ptr == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
