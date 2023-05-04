a160098dad500d736a762ca96c4021e61d4bcb5a
Evans Sese <sese_evans@yahoo.com>
2023-05-04 10:43:09 -0700

    [Feature] Added function that coverts binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - Converts binary to unsigned int
+ * @b: Pointer to the binary
+ *
+ * Return: Returns the unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	int i;
+	
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+			res = res << 1;
+		else if (b[i] == '1')
+			res = (res << 1) | 1;
+		else
+			return (0);
+	}
+	return (res);
+}
