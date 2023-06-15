201e977285eb03b134bb891a495dbec8c7b841ec
Evans Sese <sese_evans@yahoo.com>
2023-06-15 12:01:00 -0700

    [Feature] Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - Function to free list
+ * @head: Pointer to head
+ *
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	while (current != NULL)
+	{
+		free(current);
+		current = current->next;
+	}
+}
