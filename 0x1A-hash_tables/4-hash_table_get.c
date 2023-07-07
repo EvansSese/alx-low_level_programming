7f12afb8a929da80d6098042c9a37e3d99b35c1c
Evans Sese <sese_evans@yahoo.com>
2023-07-07 06:10:54 -0700

    [Feature] Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - Retrieves value of key
+ * @ht: Hash table
+ * @key: Key
+ *
+ * Return: Value retrieved
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
