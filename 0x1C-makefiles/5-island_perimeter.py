a940da37af4f636c3fbe2e9855e0897e99cb0bee
Evans Sese <sese_evans@yahoo.com>
2023-08-23 15:00:46 -0700

    [Fix] Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,11 +10,11 @@ def island_perimeter(grid):
     size = 0
 
     for k in range(height):
-        for l in range(width):
-            if grid[k][l] == 1:
+        for j in range(width):
+            if grid[k][j] == 1:
                 size += 1
-                if (l > 0 and grid[k][l - 1] == 1):
+                if (j > 0 and grid[k][j - 1] == 1):
                     edges += 1
-                if (k > 0 and grid[k - 1][l] == 1):
+                if (k > 0 and grid[k - 1][j] == 1):
                     edges += 1
     return (size * 4 - edges * 2)
