60fc14994010a1a47723b3ea0e7d9dceeb0863fa
Evans Sese <sese_evans@yahoo.com>
2023-08-23 14:10:53 -0700

    [Feature] Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pyhon3
+""" This function finds perimeter of island """
+
+
+def island_perimeter(grid):
+    """ Works the perimeter of the island """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for k in range(height):
+        for l in range(width):
+            if grid[k][l] == 1:
+                size += 1
+                if (l > 0 and grid[k][l - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[k - 1][l] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
